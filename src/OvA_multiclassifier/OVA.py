665cb202596e7829d9328c6c12d6900ccb3c0f53
konaoki <kon.aoki.us@gmail.com>
2020-05-08 03:33:19 -0700

    ova code added, untested

--- src/OvA_multiclassifier/OVA.py
+++ src/OvA_multiclassifier/OVA.py
@@ -4,7 +4,19 @@ class OVA:
     def __init__(self, model):
         self.model=model
     def fit(self, data, labels):
+        self.data=data
+        self.labels=labels
+        label_lookup=dict()
         ulabels=np.unique(labels)
+        nfeat=len(ulabels)
+        models=np.array(nfeat)
         for i,ul in enumerate(ulabels):
-            
-            self.model.fit(data,labels)
+            label_lookup[ul]=i
+            blabels = np.where(labels==ul,1,0) #binary labels
+            self.model.fit(data,blabels)
+            models.append(self.model.copy())
+        self.label_lookup=label_lookup
+        self.models=models
+    def predict(self,sample):
+        model=models[self.label_lookup[sample[1]]]
+        return model.predict(sample)
