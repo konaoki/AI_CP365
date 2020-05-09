fixed lagrangian derivative and added weight vector for visualization of the hyperplane

src/SVM/SVM.py
@@ -18,7 +18,8 @@ class SVM:
                 prediction=0
                 for j in range(nex):
                     sum+=lm[j]*labels[j]*np.dot(data[i],data[j])
-                of=1-labels[i]*sum
+                of=1-0.5*labels[i]*sum
+                #print(of)
                 lm[i]=lm[i]+(lr/np.dot(data[i],data[i]))*of
             if l==epochs-1:
                 biases=np.zeros(nex)
@@ -31,6 +32,10 @@ class SVM:
         self.lm=lm
         self.data=data
         self.labels=labels
+        weights=np.zeros(nfeat)
+        for i in range(nex):
+            weights=np.add(weights,lm[i]*labels[i]*data[i])
+        self.weights=weights
     def predict(self, sample, raw=0, bias=None, lm=[], data=[], labels=[]):
         prediction=0
         if bias==None:

src/SVM/SVMTester.py
@@ -8,7 +8,7 @@ class SVMTester:
     nfeat = 2
     #dg = DataGenerator.DataGenerator(nfeat,ndata,0.01)
     #data = dg.generate()
-    #data.to_csv('2d_test_2.data', index=False,header=False)
+    #data.to_csv('2d_test.data', index=False,header=False)
     data=pd.read_csv('2d_test.data', header = None, encoding='utf-8')
     pData = data.iloc[:ndata, :nfeat].values
     labels = data.iloc[:ndata,nfeat].values
@@ -23,8 +23,9 @@ class SVMTester:
             nerror+=1
 
     print(nerror)
-    #print(model.ws)
-    #x = np.linspace(-100, 100, 1000)
-    #plt.plot(x,-x*model.ws[1]/model.ws[0])
+    ws=model.weights
+    print(ws)
+    x = np.linspace(np.min(pData[:,0]), np.max(pData[:,0]), 1000)
+    plt.plot(x,-x*ws[0]/ws[1])
     plt.scatter(pData[:,0],pData[:,1],c=labels)
-    #plt.show()
+    plt.show()