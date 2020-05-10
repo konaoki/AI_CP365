49321fcb139ee0fd4bf7fba62b6a65c69933401a
konaoki <kon.aoki.us@gmail.com>
2020-05-09 22:15:45 -0700

    can now generate data with specified error

--- src/SVM/DataGenerator.py
+++ src/SVM/DataGenerator.py
@@ -19,9 +19,12 @@ class DataGenerator:
             data[i,:d]=np.add(data[i,:d],sign*dir*separation)
             data[i,d]=sign
         for i in range(n-nerror,n):
-            data[i,:d]=nr.random_integers(1,n/10)*nr.randn(d)
-            sign=np.sign(np.dot(dir,data[i,:d]))
-            #data[i,:d]=np.add(data[i,:d],sign*dir*separation)
-            data[i,d]=-1*sign
+            data[i,:d]=nr.random_integers(1,n/20)*nr.randn(d)
+            sign=0
+            if nr.random()>0.5:
+                sign=1
+            else:
+                sign=-1
+            data[i,d]=sign
 
         return pd.DataFrame(data)
